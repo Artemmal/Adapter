4d773180320cb8ff02022a05663677ef7561bd1f
Artemmal <45432448+Artemmal@users.noreply.github.com>
2019-11-19 15:24:26 +0200

    Add files via upload

---
+++ Program(7).cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HW23Adapter
+{
+    abstract class Sensor
+    {
+        public virtual void DegreesInFahrenheit()
+        {
+
+        }
+    }
+    class FahrenheitSensor : Sensor
+    {
+        public override void DegreesInFahrenheit()
+        {
+            Console.WriteLine("Fahrenheit degrees");
+        }
+    }
+    class CelsiusSensor
+    {
+        public void DegreesInCelsius()
+        {
+            Console.WriteLine("Celsius Degrees");
+        }
+    }
+    class Adapter : Sensor
+    {
+        CelsiusSensor celsiusSensor;
+        public Adapter(CelsiusSensor cs)
+        {
+            celsiusSensor = cs;
+        }
+        public override void DegreesInFahrenheit()
+        {
+            celsiusSensor.DegreesInCelsius();
+        }
+    }
+    class ClientIndicatorOfTemperature
+    {
+        public void Indicate(Sensor sensor)
+        {
+            sensor.DegreesInFahrenheit();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClientIndicatorOfTemperature client = new ClientIndicatorOfTemperature();
+            FahrenheitSensor fahrenheitSensor = new FahrenheitSensor();
+            client.Indicate(fahrenheitSensor);
+            CelsiusSensor celsiusSensor = new CelsiusSensor();
+            Sensor adapter = new Adapter(celsiusSensor);
+            client.Indicate(adapter);
+        }
+    }
+}
